70e9fee4041139cefcc778890b35fc8977751ce9
Daniel Erb <erb.daniel@outlook.com>
2023-04-27 13:03:46 -0400

    Update all search pages to add favorite function

--- react-project/src/pages/ClosestSearch.jsx
+++ react-project/src/pages/ClosestSearch.jsx
@@ -42,21 +42,15 @@ export function ClosestSearch() {
 								{brewery.postal_code}
 							</td>
 							<td>
-								<button className="btn btn-primary">Add</button>
+								<button
+									onClick={() => axAddFavorite(brewery)}
+									className="btn btn-primary"
+								>
+									Add
+								</button>
 							</td>
 						</tr>
 					))}
-					{/* <tr>
-						<th scope="row">2</th>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<th scope="row">3</th>
-						<td colspan="2">Larry the Bird</td>
-						<td>@twitter</td>
-					</tr> */}
 				</tbody>
 			</table>
 		</div>
