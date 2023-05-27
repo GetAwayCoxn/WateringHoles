cfe2376b456b29c805c6f7b7d6661ab4d200eca3
Daniel Erb <erb.daniel@outlook.com>
2023-05-27 10:43:23 -0400

    register models to the django admin site

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core_app.models import Core_User, Brewery, Favorite
 
-# Register your models here.
+admin.site.register(Core_User)
+admin.site.register(Brewery)
+admin.site.register(Favorite)
